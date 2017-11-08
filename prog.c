5bc910feacc6386cb4c313f07514a0665b246262
Pius Surendralal <pius.surendralal@gmail.com>
2017-11-08 09:59:30 +0000

    make array pointers global

--- prog.c
+++ prog.c
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#define DEBUG
+
 struct program_args
 {
     char *filename;   // -f
@@ -15,8 +17,6 @@ struct program_args
 
 struct thread_args
 {
-    double **a;
-    double **b;
     int cells_to_relax;
     int start_row;
     int start_col;
@@ -24,6 +24,8 @@ struct thread_args
 
 pthread_barrier_t barrier;
 char is_done = 1, should_continue = 1;
+double **a = NULL;
+double **b = NULL;
 
 void swap_array(double ***a, double ***b)
 {
@@ -48,7 +50,7 @@ void print_array(double **a)
  * Reads the values of the initial array from file into 'a' and 'b'.
  * Initially both 'a' and 'b' will be the same.
  */
-void read_array(double **a, double **b)
+void read_array()
 {
     FILE *file = fopen(args.filename, "r");
     for (int i = 0; i < args.dimension; i++)
@@ -81,14 +83,14 @@ void relax_section(struct thread_args *thread_args)
             if (cells_relaxed == thread_args->cells_to_relax)
                 return;
 
-            double north = thread_args->a[i - 1][j];
-            double south = thread_args->a[i + 1][j];
-            double west = thread_args->a[i][j - 1];
-            double east = thread_args->a[i][j + 1];
+            double north = a[i - 1][j];
+            double south = a[i + 1][j];
+            double west = a[i][j - 1];
+            double east = a[i][j + 1];
 
-            thread_args->b[i][j] = (north + east + south + west) / 4;
+            b[i][j] = (north + east + south + west) / 4;
 
-            double precision = fabs(thread_args->b[i][j] - thread_args->a[i][j]);
+            double precision = fabs(b[i][j] - a[i][j]);
             if (precision > args.precision)
             {
                 // this is a datarace between threads that need to continue
@@ -111,11 +113,7 @@ void relax_section_thread(struct thread_args *thread_args)
         // wait for other threads to complete their part
         pthread_barrier_wait(&barrier);
 
-        // ensures results are always stored in 'a',
-        // at the start of next iteration
-        swap_array(&thread_args->a, &thread_args->b);
-
-        // wait until main thread prints the array
+        // wait until main thread prepares data for next iteration
         // and checks whether we need to continue
         pthread_barrier_wait(&barrier);
     }
@@ -131,13 +129,13 @@ void relax_section_main(struct thread_args *thread_args)
         pthread_barrier_wait(&barrier);
     }
 
-    // ensures results are always stored in 'a',
-    // at the start of next iteration
-    swap_array(&thread_args->a, &thread_args->b);
+    // swap so that results are in 'a' for next iteration
+    // other threads will be waiting at second barrier
+    swap_array(&a, &b);
 
-    // print while other threads are waiting at second barrier
-    printf("\n");
-    print_array(thread_args->a);
+#ifdef DEBUG
+    print_array(a);
+#endif
 }
 
 void relax_array(struct thread_args *all_threads_args)
@@ -199,19 +197,17 @@ void relax_array(struct thread_args *all_threads_args)
     }
 }
 
-void alloc_memory(double ***a_out, double ***b_out, double **a_buf_out, double **b_buf_out, struct thread_args **all_threads_args_out)
+void alloc_memory(double **a_buf_out, double **b_buf_out, struct thread_args **all_threads_args_out)
 {
-    // allocate memory for each thread's arguments
+    // memory for each thread arguments
     struct thread_args *all_threads_args = malloc((unsigned long)args.threads * sizeof(struct thread_args));
 
-    // allocate memory for pointers which will point to the "nested arrays" inside the "main 2d arrays"
-    // one to read from, and one to write to, so that neighbors arent affected
-    double **a = malloc((unsigned long)args.dimension * sizeof(double *));
-    double **b = malloc((unsigned long)args.dimension * sizeof(double *));
+    // memory for array
+    a = malloc((unsigned long)args.dimension * sizeof(double *));
+    b = malloc((unsigned long)args.dimension * sizeof(double *));
 
-    // allocate memory for pointer to the whole "2d array"
-    double *a_buf = malloc((unsigned long)(args.dimension * args.dimension) * sizeof(double));
-    double *b_buf = malloc((unsigned long)(args.dimension * args.dimension) * sizeof(double));
+    double *a_buf = malloc( (unsigned long)(args.dimension * args.dimension) * sizeof(double));
+    double *b_buf = malloc( (unsigned long)(args.dimension * args.dimension) * sizeof(double));
 
     if (a == NULL || b == NULL || a_buf == NULL || b_buf == NULL || all_threads_args == NULL)
     {
@@ -219,22 +215,21 @@ void alloc_memory(double ***a_out, double ***b_out, double **a_buf_out, double *
         exit(EXIT_FAILURE);
     }
 
-    // each a[i] points to start of a "nested array"
+    // each a[i] points to start of a row
     for (int i = 0; i < args.dimension; i++)
-    {
+{
         a[i] = a_buf + args.dimension * i;
         b[i] = b_buf + args.dimension * i;
     }
 
-    *a_out = a;
-    *b_out = b;
+    // TODO: fix this
     *a_buf_out = a_buf;
     *b_buf_out = b_buf;
 
     *all_threads_args_out = all_threads_args;
 }
 
-void alloc_work(double **a, double **b, struct thread_args *all_threads_args)
+void alloc_work(struct thread_args *all_threads_args)
 {
     // each thread will relax n cells, where n is cells_to_relax
     int cells_to_relax = (args.dimension - 2) * (args.dimension - 2) / args.threads;
@@ -249,9 +244,6 @@ void alloc_work(double **a, double **b, struct thread_args *all_threads_args)
 
     for (int i = 0; i < args.threads; i++)
     {
-        all_threads_args[i].a = a;
-        all_threads_args[i].b = b;
-
         all_threads_args[i].start_row = row + 1;
         all_threads_args[i].start_col = col + 1;
 
@@ -312,17 +304,20 @@ int main(int argc, char *argv[])
 {
     process_args(argc, argv);
 
-    double **a = NULL;
-    double **b = NULL;
     double *a_buf = NULL;
     double *b_buf = NULL;
     struct thread_args *all_threads_args = NULL;
 
-    alloc_memory(&a, &b, &a_buf, &b_buf, &all_threads_args);
-    alloc_work(a, b, all_threads_args);
+    alloc_memory(&a_buf, &b_buf, &all_threads_args);
+    alloc_work(all_threads_args);
 
-    read_array(a, b);
+    read_array();
+
+    printf("Input array:\n");
+    print_array(a);
     relax_array(all_threads_args);
+    printf("Output array:\n");
+    print_array(a);
 
     // deallocate memory
     free(a);
